a4fa950b08981d53a448e929383c973e28e8f5b5
Ageu Silva <ageu.tecnologia@gmail.com>
2020-06-21 17:27:07 -0300

    first commit

---
+++ Variaves_e_Dados.js
@@ -0,0 +1,16 @@
+var nome = 'Ageu';
+var idade = 21;
+var peso = 93;
+var humano = true;
+
+var alunos = ['Ageu','Luan','Vitor'];
+
+//objeto
+var  aluno = {
+    nome: 'Ageu',
+    idade: 21,
+    peso: 93,
+    humano: true
+};
+
+console.log(aluno)
